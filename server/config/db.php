2063cf5faa095b4c04e5a9bcc035b766ae265df5
JuraPapunitsky <jurapapunitsky@gmail.com>
2017-07-20 15:59:55 +0300

    authenticate rest api

--- server/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=autocaller',
-    'username' => 'root',
-    'password' => '012345',
-    'charset' => 'utf8',
-];
