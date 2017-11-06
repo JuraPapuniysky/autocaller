751dc109777cd2ea7dc38aedddcdbba5f4fc4a90
JuraPapuniysky <jurapapunitsky@gmail.com>
2017-11-06 14:03:50 +0200

    fix bugs with conference

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,9 +9,9 @@ import { List } from "../components/lists/list";
 export class DataService {
 
 
-  private url = 'http://10.109.36.195:5050';
+  private url = 'http://10.109.71.239:5050';
 
-  private asteriskUrl = 'http://10.109.36.195:4242';
+  private asteriskUrl = 'http://10.109.71.239:4242';
   private token: string;
 
 
